0756fe3cc0b313e2ffac072bbfb0fe3ccac6b1ea
UBC Student <kannana1@pender.students.cs.ubc.ca>
2021-11-18 17:37:12 -0800

    Started working on PA3

--- pa3/stats.cpp
+++ pa3/stats.cpp
@@ -1,9 +1,22 @@
 
 #include "stats.h"
 
-stats::stats(PNG & im){
+// initialize the private vectors so that, for each color channel, 
+// (x,y) is the cumulative sum of the the color values from (0,0)
+// to (x,y). 
+// Note that the hue (h) value of each pixel is represented by
+// its cartesian coordinates: X = cos(h) and Y = sin(h). 
+// This is done to simplify distance and average computation.
+stats::stats(PNG & im) {
+    //there are private vectors: sumHueX, sumHueY, sumSat, sumLum, 
 
-/* your code here! */
+//background:
+    //first figure out how hue is translated into cartesian coordinates
+    //then figure out base cases. 
+        //A 1x1 (1 pixel) img, with certain HSLA values, write a test to see if this is initialised properly 
+        //A 2x2 (4 pixel) img, w/ certain HSLA vals, write a test to see if init properly
+
+//implementation:
 
 }
 
